fbc3743b5a85e63d4af4ddb282db61fa70fdac6c
Javier Hdez. <movidroid@gmail.com>
2015-04-24 09:44:43 +0100

    XBeeThread commit

---
+++ JetRemote/src/com/jetremote/util/XBeeThread.java
@@ -0,0 +1,31 @@
+package com.jetremote.util;
+
+import com.digi.xbee.api.RemoteXBeeDevice;
+import com.digi.xbee.api.exceptions.TimeoutException;
+import com.digi.xbee.api.exceptions.XBeeException;
+import com.digi.xbee.api.io.IOLine;
+import com.digi.xbee.api.io.IOValue;
+
+public class XBeeThread extends Thread{
+	private RemoteXBeeDevice device;
+	private IOValue value;
+
+	public XBeeThread(RemoteXBeeDevice device, IOValue value) {
+		this.device = device;
+		this.value = value;
+	}
+
+	public Thread newThread(Runnable r) {
+
+		try {
+			device.setDIOValue(IOLine.DIO0_AD0, value);
+		} catch (TimeoutException e) {
+			System.out.println("Error trying to set the DIO value >> " +  e.toString());
+		} catch (XBeeException e) {
+			System.out.println("Error trying to set the DIO value >> " +  e.toString());
+		}
+		
+		return null;
+	}
+
+}
